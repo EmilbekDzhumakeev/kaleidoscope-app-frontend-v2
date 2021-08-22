165d5c5da6db606bce58d17fb41b09f0065d4038
EmilbekDzhumakeev <emilbek.dzhumakeev17@gmail.com>
2021-08-22 02:11:07 -0400

    commented out Button element for future development

--- src/components/bannerActionBar/bannerActionBar.js
+++ src/components/bannerActionBar/bannerActionBar.js
@@ -7,11 +7,11 @@ const BannerActionBar = (props) => {
    return (
       <div className="banner-action-bar">
          <div className="user-info">Hi, {props.loggedInUser.name.split(' ')[0]}</div>
-         <div className='button-md'>
+         {/* <div className='button-md'>               /// this area can be used to set current user to view different user profile and add current user to friend list
       
             <div className='request-text'></div>
             {props.loggedInUser.email !== props.currentUser.email }
-         </div>
+         </div> */}
 
          <div className="login"><button className="button-md" onClick={props.handleLoginAvatarClick}>Log Out</button></div>
       </div>
